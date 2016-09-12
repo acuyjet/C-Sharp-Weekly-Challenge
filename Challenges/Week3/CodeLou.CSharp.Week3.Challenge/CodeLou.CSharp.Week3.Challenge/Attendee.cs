840418fa93ee879a854ae97bfbc00622c0a06b92
Allison.Cuyjet <allison@cuyjet.com>
2016-09-11 21:42:41 -0400

    Added Appointments, Attendee, and Meetings classes

---
+++ Challenges/Week3/CodeLou.CSharp.Week3.Challenge/CodeLou.CSharp.Week3.Challenge/Attendee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLou.CSharp.Week3.Challenge
+{
+    class Attendee
+    {
+        public string Name { get; set; }
+    }
+}
--- Challenges/Week3/CodeLou.CSharp.Week3.Challenge/CodeLou.CSharp.Week3.Challenge/CalendarItemBase.cs
+++ Challenges/Week3/CodeLou.CSharp.Week3.Challenge/CodeLou.CSharp.Week3.Challenge/CalendarItemBase.cs
@@ -1,7 +1,14 @@
-﻿namespace CodeLou.CSharp.Week3.Challenge
+﻿using System;
+
+namespace CodeLou.CSharp.Week3.Challenge
 {
 	public abstract class CalendarItemBase
 	{
 		public int Id { get; set; }
-	}
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Location { get; set; }
+    }
 }
